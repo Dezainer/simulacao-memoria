0dbf9ff20f2416991f7a1d8ec9c7afbf6b319a41
dezainer <gameboymasterz@gmail.com>
2016-10-26 01:26:02 -0300

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var webpack = require('webpack');
+ 
+module.exports = {
+  entry: [
+          './components/appbar.js',
+          './model/management.js'
+  ],
+  output: { path: __dirname, filename: './public/bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
