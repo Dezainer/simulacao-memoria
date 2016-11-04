ab0287add7257b2191943e07e9ad75b853858c89
dezainer <gameboymasterz@gmail.com>
2016-11-04 03:30:56 +0000

    A very tiny bit of progress (its 3:30am and im tired)

--- components/appbar.js
+++ components/appbar.js
@@ -58,19 +58,37 @@ var apps = [
 	}
 ];
 
+var open = [
+	{
+		"name": "Chrome",
+		"size": 4
+	},
+	{
+		"name": "Spotify",
+		"size": 2
+	}
+];
+
 var App = React.createClass({
+
+	handleClick: function (name, size) {
+		return function () {
+			open.push({"name": name, "size": size});
+			console.log(open);
+		}.bind(this);
+	},
 
 	render: function () {
 		return(	
 			<ul>
 				{
-					this.props.apps.map(function (apps, i) {
+					this.props.apps.map(function (apps, index) {
 						return(
-							<li key={i}>
+							<li key={index} onClick={this.handleClick(apps.name, apps.size)}>
 								<img className="app" src={apps.icon} width={apps.dimensions} height={apps.dimensions}></img>
 							</li>
 						);
-					})
+					}, this)
 				}
 			</ul>
 		)
@@ -95,4 +113,76 @@ var AppBar = React.createClass({
 ReactDOM.render(
 	<AppBar/>,
 	document.getElementById('appbar')
+);
+
+
+
+
+
+function inner(size) {
+	var inner = [];
+
+	for (var i = 0; i < size; i++) {
+		inner.push(<li>c{i}</li>);
+	}
+
+	return inner;
+}
+
+var Manager = React.createClass({
+
+	getInitialState: function () {
+		return{
+			open: []
+		}
+	},
+
+	componentWillMount: function () {
+		this.setState({open: this.props.open});
+	},
+
+	componentWillReceiveProps: function (newProps) {
+		this.setState({open: newProps.open});
+	},
+
+	render: function () {
+
+		var oList = this.state.open.map(function (open, i) {
+			return(
+				<div>
+					<ul className="outter-list">
+						<li className="name"><div className="seta"><span>></span></div>{open.name}</li>
+						<ul className="inner-list">
+							{inner(open.size)}
+						</ul>
+					</ul>
+				</div>
+			);
+		});
+
+		return(
+			<div className="window" id="drag">
+				<div className="header">
+					<span className="button" id="close"> </span>
+					<span className="button" id="max"> </span>
+					<span className="button" id="min"> </span>
+				</div>
+
+				<div className="container">
+					<h1>Memória Virtual</h1>
+					<p>Mostra como o sistema operacional ve a memória</p>
+
+					<div className="content">
+						{oList}
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+});
+
+ReactDOM.render(
+	<Manager open={open}/>,
+	document.getElementById('manager')
 );
